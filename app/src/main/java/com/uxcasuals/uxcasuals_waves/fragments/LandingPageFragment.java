de35525d53dd4433267544fc8f98e7e8af4c2a5a
Dhakchianandan <dhakchianandan@gmail.com>
2015-09-24 20:39:37 +0530

    Remove default configurations

---
+++ app/src/main/java/com/uxcasuals/uxcasuals_waves/fragments/LandingPageFragment.java
@@ -0,0 +1,31 @@
+package com.uxcasuals.uxcasuals_waves.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uxcasuals.uxcasuals_waves.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LandingPageFragment extends Fragment {
+
+
+    public LandingPageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landing_page, container, false);
+    }
+
+
+}
